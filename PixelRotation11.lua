8ca288cfcd671541122df363b5a741dc4726c097
liantian <liantian.me+code@gmail.com>
2024-10-18 16:02:16 +0800

    Update GetPlayerAuraCount

--- PixelRotation11.lua
+++ PixelRotation11.lua
@@ -1,7 +1,5 @@
 
 
-local GetPlayerAuraBySpellID = C_UnitAuras.GetPlayerAuraBySpellID
-
 
 local frame = CreateFrame("Frame", "PixelRotation11Frame", UIParent)
 frame:SetSize(64, 64)
@@ -44,36 +42,33 @@ function are3EnemiesInRange()
    return (inRange > 2)
 end
 
-function PR_get_buff_count(spellID)
-    -- buff层数
-    -- local _, _, _, count, _, duration, expires, _, _, _, spellID, _, _, _, _, _, _, _, _ =  UnitBuff("player", GetSpellInfo(db_buff[buff_name]))
-    -- local _, _, _, count, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _ =  UnitBuff("player", GetSpellInfo(db_buff[buff_name]))
-    local name, icon, count, dispelType, duration, expirationTime, source, isStealable, nameplateShowPersonal,spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod, _ = GetPlayerAuraBySpellID(spellID)
-    -- 如果层数是nil，那就是无buff，返回0。
-    if count == nil then
-        return 0
+local function GetPlayerAuraCount(spellID)
+  local data = C_UnitAuras.GetPlayerAuraBySpellID(spellID)
+  local count 
+  if data then
+      count = data.charges
+  else
+    -- 如果是nil，那就是无buff，返回0。
+     return 0
+  end
+
+  -- 如果是0层，他们可能没有层数设置。
+  if data.charges == 0 then
+    count = 1
+  end
+
+  local currentTime = GetTime()
+  local timeUntilExpire = data.expirationTime - currentTime
+
+
+  -- 如果坚持不到下一个gcd结束，则是0 
+  local spellCooldownInfo = C_Spell.GetSpellCooldown(61304)
+  if spellCooldownInfo then
+    if  data.expirationTime > 0) and (data.expirationTime < spellCooldownInfo.startTime + spellCooldownInfo.duration) then
+        count = 0
     end
+  end
+      
+  return count
 
-    -- 如果这是个读秒的buff，持续不到下次施法，则返回0
-    if (expires > 0) and ((expires - cast.time_remaining()) <= 0) then
-        return 0
-    end
-
-    -- 如果层数是0，则表明这是不叠层的buff，返回1，1层
-    if count == 0 then
-        count = 1
-    end
-
-    -- 愤怒buff考虑当前施法
-    if (buff_name =="solar_wrath") and (current_casting == db_spell["solar_wrath"]) then
-        count = count - 1
-    end
-
-    -- 愤怒buff考虑当前施法
-    if (buff_name =="lunar_strike") and (current_casting == db_spell["lunar_strike"]) then
-        count = count - 1
-    end
-
-    return count
-
-end
+end --GetPlayerAuraCount
